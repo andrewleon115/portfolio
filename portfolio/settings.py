d6ded30e8085bff448b50897d7316e5764a5e8da
Amose <132036107+andrewleon115@users.noreply.github.com>
2025-06-10 10:26:01 +0530

    Added Procfile and ready for Render deployment

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -88,12 +88,12 @@ WSGI_APPLICATION = "portfolio.wsgi.application"
 #         'PORT': '3306',
 #     }
 # }
+from decouple import config
 import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': dj_database_url.parse(config('DATABASE_URL'), conn_max_age=600, ssl_require=True)
 }
-
 
 
 
