a5d911c2079dc36ebfa68893262b42f366d0e987
Amose <132036107+andrewleon115@users.noreply.github.com>
2025-06-10 10:10:46 +0530

    Added Procfile and ready for Render deployment

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = "portfolio.wsgi.application"
 import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 
