99f90110f9951d62f078b0afb8c052a4aacab911
Amose <132036107+andrewleon115@users.noreply.github.com>
2025-06-10 09:04:41 +0530

    Added Procfile and ready for Render deployment

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = "django-insecure-i_8qca7u6s*#wd%eg)&!)if(4q^drn^$m0yo@+9+p1uazfvj$)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition
@@ -50,6 +51,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = "portfolio.urls"
@@ -124,7 +127,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
